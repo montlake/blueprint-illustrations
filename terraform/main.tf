1ff00ae8309d6997ed95976a2f1f33ecb0146396
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2017-03-27 23:33:01 +0100

    Add health check configuration to ELB
    Lowers healthy threshold

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,6 +98,14 @@ resource "aws_elb" "web" {
     lb_port           = 80
     lb_protocol       = "http"
   }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 15
+  }
 }
 
 resource "aws_key_pair" "auth" {
